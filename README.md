f531bcb4fff525ba8758de7e42af0b757c4a651d
Julian Wergieluk <julian@wergieluk.com>
2019-06-03 22:40:38 +0200

    Playing with state and action spaces

--- README.md
+++ README.md
@@ -10,16 +10,27 @@ This project was done as part of the Udacity Deep Reinforcement Learning Nanodeg
 
 # Description of the environment
 
-In this environment, two agents control rackets to bounce a ball over a net. If an agent hits the ball over the net, it receives a reward of +0.1. If an agent lets a ball hit the ground or hits the ball out of bounds, it receives a reward of -0.01. Thus, the goal of each agent is to keep the ball in play.
+In this environment, two agents control rackets to bounce a ball over a net. If
+an agent hits the ball over the net, it receives a reward of +0.1. If an agent
+lets a ball hit the ground or hits the ball out of bounds, it receives a reward
+of -0.01. Thus, the goal of each agent is to keep the ball in play.
 
-The observation space consists of 8 variables corresponding to the position and velocity of the ball and racket. Each agent receives its own, local observation. Two continuous actions are available, corresponding to movement toward (or away from) the net, and jumping.
+The observation space consists of 8 variables corresponding to the position and
+velocity of the ball and racket. Each agent receives its own, local
+observation. Two continuous actions are available, corresponding to movement
+toward (or away from) the net, and jumping.
 
-The task is episodic, and in order to solve the environment, your agents must get an average score of +0.5 (over 100 consecutive episodes, after taking the maximum over both agents). Specifically,
+The task is episodic, and in order to solve the environment, your agents must
+get an average score of +0.5 (over 100 consecutive episodes, after taking the
+maximum over both agents). Specifically,
 
-After each episode, we add up the rewards that each agent received (without discounting), to get a score for each agent. This yields 2 (potentially different) scores. We then take the maximum of these 2 scores.
-This yields a single score for each episode.
+* After each episode, we add up the rewards that each agent received (without
+discounting), to get a score for each agent. This yields 2 (potentially
+different) scores. We then take the maximum of these 2 scores.
+* This yields a single score for each episode.
 
-The environment is considered solved, when the average (over 100 episodes) of those scores is at least +0.5.
+The environment is considered solved, when the average (over 100 episodes) of
+those scores is at least +0.5.
 
 # Installation instructions
 
@@ -85,7 +96,8 @@ and save the learned parameters to disk:
 python deep_tennis.py train --max-episodes 250
 ```
 
-The above command runs for 250 training episodes and saves the resulting parameters of a trained agent to the current directory.
+The above command runs for 250 training episodes and saves the resulting
+parameters of a trained agent to the current directory.
 
 # License
 
---
+++ deep_tennis.py
@@ -0,0 +1,405 @@
+# coding=utf-8
+import copy
+import datetime
+import pandas
+import numpy
+import random
+from unityagents import UnityEnvironment
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from collections import deque, namedtuple
+import click
+import time
+
+
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 256        # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR_ACTOR = 1e-3         # learning rate of the actor
+LR_CRITIC = 1e-3        # learning rate of the critic
+WEIGHT_DECAY = 0        # L2 weight decay
+EPSILON = 1.0
+EPSILON_DECAY = 1e-6
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class Actor(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=128):
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self._pi_net = torch.nn.Sequential(
+            nn.BatchNorm1d(state_size),
+            nn.Linear(state_size, fc1_units),
+            nn.BatchNorm1d(fc1_units),
+            nn.ReLU(),
+            nn.Linear(fc1_units, fc2_units),
+            nn.BatchNorm1d(fc2_units),
+            nn.ReLU(),
+            nn.Linear(fc2_units, action_size),
+            nn.Tanh()
+        )
+
+    def forward(self, state):
+        """Build an actor (policy) network that maps states -> actions."""
+
+        return self._pi_net(state)
+
+
+class Critic(nn.Module):
+    """Critic (Value) Model."""
+
+    def __init__(self, state_size, action_size, seed, state_layer_units=128, state_action_layer_units=128):
+        super().__init__()
+        self.seed = torch.manual_seed(seed)
+
+        self._q_state_net = nn.Sequential(
+            nn.BatchNorm1d(state_size),
+            nn.Linear(state_size, state_layer_units),
+            # nn.BatchNorm1d(state_layer_units),
+            nn.ReLU()
+        )
+
+        self._q_net = nn.Sequential(
+            nn.Linear(state_layer_units + action_size, state_action_layer_units),
+            # nn.BatchNorm1d(state_action_layer_units),
+            nn.ReLU(),
+            nn.Linear(state_action_layer_units, 1),
+        )
+
+    def forward(self, state, action):
+        """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+
+        state_rep = self._q_state_net(state)
+        return self._q_net(torch.cat((state_rep, action), dim=1))
+
+
+class Agent:
+    """Interacts with and learns from the environment."""
+
+    def __init__(self, state_size, action_size, random_seed):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(random_seed)
+        self.epsilon = EPSILON
+
+        # Actor Network
+        self.actor_local = Actor(state_size, action_size, random_seed).to(DEVICE)
+        self.actor_target = Actor(state_size, action_size, random_seed).to(DEVICE)
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
+
+        # Critic Network
+        self.critic_local = Critic(state_size, action_size, random_seed).to(DEVICE)
+        self.critic_target = Critic(state_size, action_size, random_seed).to(DEVICE)
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+
+        # Noise process
+        self.noise = OUNoise(action_size, random_seed)
+
+        # Replay memory
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+
+        self.copy_weights(self.actor_target, self.actor_local)
+        self.copy_weights(self.critic_target, self.critic_local)
+
+    def step(self, states, actions, rewards, next_states, dones, timestep):
+        """Save experience in replay memory, and use random sample from buffer to learn."""
+
+        for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+            self.memory.add(state, action, reward, next_state, done)
+
+        # Learn, if enough samples are available in memory
+        if len(self.memory) > BATCH_SIZE and timestep % 20 == 0:
+            for _ in range(10):
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA)
+
+    def act(self, state, add_noise=True):
+        """Returns actions for given state as per current policy."""
+
+        state = torch.from_numpy(state).float().to(DEVICE)
+
+        self.actor_local.eval()
+        with torch.no_grad():
+            action = self.actor_local(state).cpu().data.numpy()
+        self.actor_local.train()
+
+        if add_noise:
+            action += self.epsilon * self.noise.sample()
+
+        return np.clip(action, -1.0, 1.0)
+
+    def reset(self):
+        self.noise.reset()
+
+    def learn(self, experiences, gamma):
+        """Update policy and value parameters using given batch of experience tuples.
+
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+        # ---------------------------- update critic ---------------------------- #
+        # Get predicted next-state actions and Q values from target models
+        actions_next = self.actor_target(next_states)
+        Q_targets_next = self.critic_target(next_states, actions_next)
+
+        # Compute Q targets for current states (y_i)
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+
+        # Compute critic loss
+        Q_expected = self.critic_local(states, actions)
+        critic_loss = F.mse_loss(Q_expected, Q_targets)
+
+        # Minimize the loss
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1)
+        self.critic_optimizer.step()
+
+        # ---------------------------- update actor ---------------------------- #
+        # Compute actor loss
+        actions_pred = self.actor_local(states)
+        actor_loss = -self.critic_local(states, actions_pred).mean()
+
+        # Minimize the loss
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # ----------------------- update target networks ----------------------- #
+        self.soft_update(self.critic_local, self.critic_target, TAU)
+        self.soft_update(self.actor_local, self.actor_target, TAU)
+
+        # ---------------------------- update noise ---------------------------- #
+        self.epsilon -= EPSILON_DECAY
+        self.noise.reset()
+
+    def soft_update(self, local_model, target_model, tau):
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
+
+    def copy_weights(self, target, source):
+        for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(param.data)
+
+
+class OUNoise:
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
+        self.theta = theta
+        self.sigma = sigma
+        self.seed = random.seed(seed)
+        self.state = None
+        self.reset()
+
+    def reset(self):
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
+
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+        self.state = x + dx
+        return self.state
+
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.seed = random.seed(seed)
+
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
+
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(DEVICE)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(DEVICE)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(DEVICE)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(DEVICE)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(DEVICE)
+
+        return states, actions, rewards, next_states, dones
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+
+class UnityMultiAgentEnvWrapper:
+    """ This class provides gym-like wrapper around the unity environment with multiple agents """
+
+    def __init__(self, env_file: str, train_mode: bool = False):
+        self._env = UnityEnvironment(file_name=env_file)
+        self._train_mode = train_mode
+        self._brain_name = self._env.brain_names[0]
+        self._brain = self._env.brains[self._brain_name]
+
+        self._env_info = self._env.reset(train_mode=self._train_mode)[self._brain_name]
+
+        self.num_agents = len(self._env_info.agents)
+        self.state_space_dim = self._env_info.vector_observations.shape[1]
+        self.action_space_dim = self._brain.vector_action_space_size
+
+    def print(self):
+        """ Print environment information  """
+        print('Number of agents:', self.num_agents)
+        print('State space dim:', self.state_space_dim, ' Action space dim:', self.action_space_dim)
+        print('Vector observations: ', self._env_info.vector_observations.shape)
+        print(self._brain.vector_observation_space_size)
+
+    def reset(self):
+        env_info = self._env.reset(self._train_mode)[self._brain_name]
+        state = env_info.vector_observations
+        return state
+
+    def step(self, action):
+        env_info = self._env.step(action)[self._brain_name]  # send the action to the environment
+        next_states = env_info.vector_observations
+        rewards = env_info.rewards
+        dones = env_info.local_done
+        return next_states, rewards, dones, None
+
+    def close(self):
+        self._env.close()
+
+
+def train(max_episodes: int, episode_len: int = 3000):
+    """ Train the agent using a head-less environment and save the weights when done """
+
+    env = UnityMultiAgentEnvWrapper('Tennis_Linux/Tennis.x86_64', train_mode=True)
+    agent = Agent(state_size=env.state_space_dim, action_size=env.action_space_dim, random_seed=8276364)
+
+    data = []
+    scores = []
+    episode = 0
+    for episode in range(1, max_episodes):
+        states = env.reset()
+        score = numpy.zeros(env.num_agents)
+        agent.reset()
+
+        for step in range(episode_len):
+            actions = agent.act(states)
+            next_states, rewards, dones, _ = env.step(actions)
+            agent.step(states, actions, rewards, next_states, dones, step)
+
+            score += rewards
+            states = next_states
+            if numpy.any(dones):
+                break
+
+        scores.append(score.max())
+        rolling_average_score = sum(scores[-100:])/min(episode, 100)
+        data.append([score.mean(), rolling_average_score])
+        print(f'Episode {episode} ({step} steps). Final score {score.mean():.2f}. '
+              f'Average score (last 100 episodes) {rolling_average_score:.2f}. '
+              f'Replay buffer size {len(agent.memory)}.')
+
+        if episode % 10 == 0:
+            torch.save(agent.actor_local.state_dict(), f'checkpoint_actor_{episode}.pth')
+            torch.save(agent.critic_local.state_dict(), f'checkpoint_critic_{episode}.pth')
+
+    df = pandas.DataFrame(data=data, index=range(1, episode+1), columns=['score', 'rolling_avg_score'])
+    now_str = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H-%M-%S')
+    df.to_csv(f'scores-{now_str}.csv')
+
+    env.close()
+
+
+def test(actor_weights_file: str, critic_weights_file: str):
+    """ Load model parameters and run the agent """
+    env = UnityMultiAgentEnvWrapper('Tennis_Linux/Tennis.x86_64', train_mode=False)
+    agent = Agent(state_size=env.state_space_dim, action_size=env.action_space_dim, random_seed=8276364)
+
+    agent.actor_local.load_state_dict(torch.load(actor_weights_file))
+    agent.critic_local.load_state_dict(torch.load(critic_weights_file))
+    agent.actor_local.eval()
+    agent.critic_local.eval()
+
+    states = env.reset()
+    score = numpy.zeros(env.num_agents)
+
+    for step in range(3000):
+        actions = agent.act(states, add_noise=False)
+        next_states, rewards, dones, _ = env.step(actions)
+
+        score += rewards
+        states = next_states
+        if numpy.any(dones):
+            break
+
+    print(f'Final score {score.mean()}.')
+    env.close()
+
+
+@click.group()
+@click.version_option()
+def cli():
+    """ Deep Tennis -- command line interface """
+
+
+@cli.command('train')
+@click.option('--max-episodes', type=click.INT, default=251)
+def train_command(max_episodes: int):
+    """ Train the agent using a head-less environment and save the parameters when done """
+
+    env = UnityMultiAgentEnvWrapper('Tennis_Linux/Tennis.x86_64', train_mode=False)
+    env.print()
+
+    states = None
+    scores = np.zeros(env.num_agents)
+    while True:
+        time.sleep(0.5)
+        actions = np.random.randn(env.num_agents, env.action_space_dim) # select an action (for each agent)
+        actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
+        next_states, rewards, dones, _ = env.step(actions)
+
+        print('next_states', next_states)
+        print('rewards', rewards)
+        print('dones', dones)
+        scores += rewards                                  # update the score (for each agent)
+        states = next_states                               # roll over states to next time step
+        if np.any(dones):                                  # exit loop if episode finished
+            break
+
+    # train(max_episodes)
+
+
+@cli.command('test')
+@click.option('--actor-weights-file', default='checkpoint_actor.pth',
+              type=click.Path(dir_okay=False, file_okay=True, readable=True, exists=True))
+@click.option('--critic-weights-file', default='checkpoint_critic.pth',
+              type=click.Path(dir_okay=False, file_okay=True, readable=True, exists=True))
+def test_command(actor_weights_file: str, critic_weights_file: str):
+    """ Load parameters and run the agent """
+    test(actor_weights_file, critic_weights_file)
+
+
+if __name__ == '__main__':
+    cli()
